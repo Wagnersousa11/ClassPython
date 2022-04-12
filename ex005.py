c123b9d556e0697b25f841368fa5a4b1b4c5e03f
Wagner Matos <98979538+Wagnersousa11@users.noreply.github.com>
2022-04-12 15:41:48 -0300

    Update ex005.py

--- ex005.py
+++ ex005.py
@@ -1,6 +1,6 @@
 n = int(input('Digite um numero: '))
 
-ant = n-1
-suc = n+1
+'''ant = n-1
+suc = n+1'''
 
-print(f'O antecessor do numero {n} é {ant} e o sucessor é {suc}.')
+print(f'O antecessor do numero {n} é {n-1} e o sucessor é {n+1}.')
