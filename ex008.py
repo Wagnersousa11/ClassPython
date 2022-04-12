b8b67f9b36a1a729e7ef958a58862f6a98c9f90b
Wagner Matos <98979538+Wagnersousa11@users.noreply.github.com>
2022-04-12 17:12:57 -0300

    Update ex008.py

--- ex008.py
+++ ex008.py
@@ -1,6 +1,7 @@
-m = float(input('Digite a quantidade de metros a serem convertidos: '))
+m = float(input('Digite uma distancia em metros a ser convertida: '))
 
 cent = m*100
 mili = cent*10
+km = m/1000
 
-print(f'{m:.2f} metro(s) convertido(s) para centimetro(s) e milimetro(s) ficam respectivamente: {int(cent)}cm e {int(mili)}mm.')
+print(f'{m:.2f} metro(s) convertido(s) para kilometro(s), centimetro(s) e milimetro(s) ficam respectivamente: {km}km, {int(cent)}cm e {mili:.0f}mm.')
